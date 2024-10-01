cd38d704c77f9734a6336c258d720781b0e6a80a
Fan <315313325@qq.com>
2024-10-01 05:18:37 +0000

    add UI

--- rock_raper_scissors.js
+++ rock_raper_scissors.js
@@ -18,36 +18,54 @@ function getHumanChoice() {
 function playRound(humanChoice, computerChoice) {
     humanChoice = humanChoice.toLowerCase();
 
+    const res = document.querySelector("#result");
     if ((humanChoice === "scissors" && computerChoice === "paper") || (humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "rock" && computerChoice === "scissors")) {
-        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        res.textContent = `You win! ${humanChoice} beats ${computerChoice}`;
         return "You_win";
     } else if ((computerChoice === "scissors" && humanChoice === "paper") || (computerChoice === "paper" && humanChoice === "rock") || (computerChoice === "rock" && humanChoice === "scissors")) {
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        res.textContent = `You lose! ${computerChoice} beats ${humanChoice}`;
         return "You_lose";
     } else {
-        console.log("You drew!");
+        res.textContent = "You drew!";
         return "You_drew";
     }
 }
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
 
-    for (let i = 0; i < 5; i++) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-
-        const result = playRound(humanSelection, computerSelection);
-        if (result === "You_win") {
-            humanScore++;
-        } else if (result === "You_lose") {
-            computerScore++;
-        } 
-    }
-
-    console.log(`Human Score: ${humanScore}`);
-    console.log(`Computer Score: ${computerScore}`);
+function updateScore() {
+    const hS = document.querySelector("#human-score");
+    hS.textContent = humanScore;
+    const cS = document.querySelector("#computer-score");
+    cS.textContent = computerScore;
 }
 
-playGame()
+function startAgain() {
+    humanScore = 0;
+    computerScore = 0;
+    updateScore();
+}
+
+function playGame(humanSelection) {
+
+    const computerSelection = getComputerChoice();
+
+    const result = playRound(humanSelection, computerSelection);
+    if (result === "You_win") {
+        humanScore++;
+    } else if (result === "You_lose") {
+        computerScore++;
+    } 
+
+    updateScore();
+
+    if (humanScore >= 5) {
+        alert("You win the game!");
+        startAgain();
+    } else if (computerScore >= 5) {
+        alert("Computer win the game!");
+        startAgain();
+    }
+
+}
